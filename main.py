72678c255b67a52d8bfaa52a9b6138a3cea4ce91
Max-Person <max_person@mail.ru>
2021-07-01 15:20:24 +0300

    Добавлен расширенный базовый функционал
    Добавлена возможность правого клика (и соответствующий жест) , возможность задерживать оба клика, а также одновременное распознавание нескольких жестов (Например, жест для перемещения мыши и жест для клика).
    Изменено поведение перемещения мыши - добавлена "мертвая зона", перемещения пальца внутри которой не приведут к перемещению мыши на экране.

--- main.py
+++ main.py
@@ -10,38 +10,59 @@ import ctypes
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
-leftMargin = 0.1
-rightMargin = 0.1
+leftMargin = 0.2
+rightMargin = 0.2
 topMargin = 0.1
-bottomMargin = 0.4
+bottomMargin = 0.6
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (0, 100)
+fontScale = 1
+fontColor = (255, 255, 255)
+lineType = 2
+
+LEFT_CLICK = 1
+RIGHT_CLICK = 2
+POINTER = 4
 
 def distance(a, b) -> float:
   _x = a.x - b.x
   _y = a.y - b.y
   return math.sqrt(_x*_x + _y*_y)
 
-def detectGesture(landmarks, unit = float) -> str:
+def detectGesture(landmarks, unit = float) -> int:
+  result = 0
   if distance(landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP],
-              landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_PIP]) < 0.225*unit:
-    return "leftclick"
-  else:
-    if landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].y and \
-          landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y > landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y and \
-          landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y > landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].y and \
-          landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y > landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y:
-      return "point"
-    else:
-      return "none"
+              landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_PIP]) < 0.21*unit and landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y < landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y:
+    result = result | RIGHT_CLICK
+
+  if distance(landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP],
+              landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_PIP]) < 0.25*unit and landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y > landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y:
+    result = result | LEFT_CLICK
+
+    # landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y > landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y and \
+  if landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < landmarks.landmark[
+        mp_hands.HandLandmark.INDEX_FINGER_PIP].y and \
+            landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y > landmarks.landmark[
+        mp_hands.HandLandmark.RING_FINGER_MCP].y and \
+            landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y > landmarks.landmark[
+        mp_hands.HandLandmark.PINKY_MCP].y:
+    result = result | POINTER
+  return result
 
     # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     max_num_hands=1,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.7) as hands:
+    min_detection_confidence=0.8,
+    min_tracking_confidence=0.8) as hands:
   hand_unit = 0.0
+  previousX = 0
+  previousY = 0
+  leftClicked = False
+  rightClicked = False
   while cap.isOpened():
-    gesture = "none"
+    gesture = 0
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
@@ -59,35 +80,52 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    #If a hand is detected - process the gestures
     if results.multi_hand_landmarks:
       hand_landmarks = results.multi_hand_landmarks[0]
-      mp_drawing.draw_landmarks(
-         image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+      mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+      #Calculate hand measurement unit if the hand is facing the camera
       if distance(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP],
                     hand_landmarks.landmark[mp_hands.HandLandmark.WRIST])< 1.75 * distance(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP],
                                                                                           hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]) and distance(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP],
                                                                                                                                                                         hand_landmarks.landmark[mp_hands.HandLandmark.WRIST])> 1.25 * distance(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP], hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]):
-          hand_unit = distance(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP],
-                                 hand_landmarks.landmark[mp_hands.HandLandmark.WRIST])
+          hand_unit = distance(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP], hand_landmarks.landmark[mp_hands.HandLandmark.WRIST])
           gesture = detectGesture(hand_landmarks, hand_unit)
-      if gesture == "point" and results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x > leftMargin and results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x < 1-rightMargin \
-              and results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y > topMargin and results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < 1-bottomMargin:
-        ag.moveTo((results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x-leftMargin)*(1/(1-leftMargin-rightMargin))*screensize[0], (results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y-topMargin)*(1/(1-topMargin-bottomMargin))*screensize[1], 0.1)
-      if gesture == "leftclick":
-        ag.click()
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (0, 100)
-    fontScale = 1
-    fontColor = (255, 255, 255)
-    lineType = 2
+      #Check for left click
+      if (gesture & LEFT_CLICK) == LEFT_CLICK and leftClicked == False:
+        ag.mouseDown(button='left')
+        leftClicked = True
+      else:
+        if (gesture & LEFT_CLICK) != LEFT_CLICK and leftClicked == True:
+            ag.mouseUp(button='left')
+            leftClicked = False
 
-    cv2.putText(image, gesture + " " + str(hand_unit),
+      #Check for right click
+      if (gesture & RIGHT_CLICK) == RIGHT_CLICK and rightClicked == False:
+        ag.mouseDown(button='right')
+        rightClicked = True
+      else:
+        if (gesture & RIGHT_CLICK) != RIGHT_CLICK and rightClicked == True:
+           ag.mouseUp(button='right')
+           rightClicked = False
+
+      #Check for pointer
+      if (gesture & POINTER) == POINTER and hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x > leftMargin and hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x < 1-rightMargin \
+              and hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y > topMargin and hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < 1-bottomMargin:
+        if abs(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x - previousX) > (1-rightMargin-leftMargin)/180 and abs(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y - previousY) > (1-topMargin-bottomMargin)/180:
+            ag.moveTo((hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x-leftMargin)*(1/(1-leftMargin-rightMargin))*screensize[0], (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y-topMargin)*(1/(1-topMargin-bottomMargin))*screensize[1], 0.1, ag.easeOutQuad)
+            previousX = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x
+            previousY = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
+
+    """cv2.putText(image, str(gesture) + " " + str(hand_unit),
                 bottomLeftCornerOfText,
                 font,
                 fontScale,
                 fontColor,
-                lineType)
+                lineType)"""
 
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
